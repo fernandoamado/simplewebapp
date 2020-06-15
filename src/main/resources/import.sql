36f8364f2c36f46725ca8f4049b9b81120f4748d
Fernando <fp.amado@icloud.com>
2020-06-15 16:41:39 +0100

    version 1.2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
---insert into user (id, name, cars_quantity) values (1, 'Pedro', 2);
---insert into user (id, name, cars_quantity) values (2, 'Maria', 3);
---insert into user (id, name, cars_quantity) values (3, 'Ricardo', 1);
+insert into user (id, name, cars_quantity) values (1, 'Pedro', 2);
+insert into user (id, name, cars_quantity) values (2, 'Maria', 3);
+insert into user (id, name, cars_quantity) values (3, 'Ricardo', 1);
 
 -- not used, because its on update mode
